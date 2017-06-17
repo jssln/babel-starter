79ed5406694c31d713aa4f9c46c9be7c837ba2f2
Jess Lin <perdendosi@gmail.com>
2017-06-17 11:24:05 -0700

    Add babel-preset-flow

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // With babel-cli installed, you get the 'babel' and 'babel-node' binaries in ./node_modules/.bin.
-// You can see this used in the "scripts"=>"demo" section of the package.json. 
+// You can see this used in the "scripts"=>"demo" section of the package.json.
 var http = require('http');
 
 // We also installed babel-preset-env and specified we wanted to use it in the .babelrc.
-// As of June 2017, this gives us Javascript features from es2015, es2016, es2017, and latest. 
+// As of June 2017, this gives us Javascript features from es2015, es2016, es2017, and latest.
 import path from 'path';
 
 async function doAThing() {
@@ -13,3 +13,6 @@ async function doAThing() {
 }
 Promise.resolve(doAThing());
 
+// After adding babel-preset-flow, we can declare Flow types and have them stripped out by Babel.
+type SampleType = { sampleField: number };
+const aThing: SampleType = { sampleField: 1 };
